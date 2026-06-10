6d121544121ffaaae13edea87f73abd7dbc11ddc
johannes .D <j.duschl@tu-berlin.de>
2026-06-10 18:55:12 +0200

    init multi project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    java
 }
 
 group = "org.example"
@@ -9,12 +9,54 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+subprojects {
+    apply(plugin = "java")
+
+    group = rootProject.group
+    version = rootProject.version
+
+    repositories {
+        mavenCentral()
+    }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
+tasks.register("initModules") {
+    doLast {
+        val modules = listOf("changeDetector", "weatherSensor", "oilSensor", "receiver")
+
+        modules.forEach { m ->
+            val dir = file(m)
+
+            file("$dir/src/main/java").mkdirs()
+            file("$dir/src/test/java").mkdirs()
+
+            val buildFile = file("$dir/build.gradle.kts")
+            if (!buildFile.exists()) {
+                buildFile.writeText("""
+plugins {
+    java
 }
 
-tasks.test {
-    useJUnitPlatform()
+repositories {
+    mavenCentral()
+}
+""".trimIndent())
+            }
+        }
+    }
 }
