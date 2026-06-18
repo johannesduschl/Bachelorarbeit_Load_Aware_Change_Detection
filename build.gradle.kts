cf9938aa24cde9c574ea2cdea37eff6924dd649d
johdu <johduschl@gmail.com>
2026-06-18 11:21:00 +0200

    docker compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,14 @@
-plugins {
-    java
-}
-
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
     apply(plugin = "java")
-
-    group = rootProject.group
-    version = rootProject.version
-
-    repositories {
-        mavenCentral()
-    }
 
     java {
         toolchain {
@@ -25,38 +16,13 @@ subprojects {
         }
     }
 
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    }
-
-    tasks.test {
-        useJUnitPlatform()
-    }
-}
-tasks.register("initModules") {
-    doLast {
-        val modules = listOf("changeDetector", "weatherSensor", "oilSensor", "receiver")
-
-        modules.forEach { m ->
-            val dir = file(m)
-
-            file("$dir/src/main/java").mkdirs()
-            file("$dir/src/test/java").mkdirs()
-
-            val buildFile = file("$dir/build.gradle.kts")
-            if (!buildFile.exists()) {
-                buildFile.writeText("""
-plugins {
-    java
-}
-
-repositories {
-    mavenCentral()
-}
-""".trimIndent())
-            }
-        }
     }
 }
